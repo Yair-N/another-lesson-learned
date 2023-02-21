820e71fa5f950120445584a88c0a7658e4355025
Yair <yair.notkovich@gmail.com>
2023-02-21 12:51:13 +0200

    CORS

--- node/Nasa-Project/server/src/app.js
+++ node/Nasa-Project/server/src/app.js
@@ -1,8 +1,21 @@
 const express = require('express');
-
+const cors = require('cors')
 const planetsRouter = require('./routes/planets/planets.router')
 
 const app = express();
+
+const whitelist = ['http://localhost:3000']
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
+
+app.use(cors(corsOptions))
 
 app.use(planetsRouter)
 app.use(express.json())
