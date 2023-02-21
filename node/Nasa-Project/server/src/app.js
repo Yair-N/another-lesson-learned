82d7aeba003fe9bf7415318a809827894ad318ce
Yair <yair.notkovich@gmail.com>
2023-02-21 17:16:45 +0200

    scripts in root package.json

--- node/Nasa-Project/server/src/app.js
+++ node/Nasa-Project/server/src/app.js
@@ -1,24 +1,15 @@
 const express = require('express');
 const cors = require('cors')
+const path = require('path')
 const planetsRouter = require('./routes/planets/planets.router')
 
 const app = express();
 
-const whitelist = ['http://localhost:3000']
-const corsOptions = {
-    origin: function (origin, callback) {
-        if (whitelist.indexOf(origin) !== -1) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'))
-        }
-    }
-}
-
-app.use(cors(corsOptions))
+app.use(cors({ origin: 'http://localhost:3000' }))
 
 app.use(planetsRouter)
 app.use(express.json())
+app.use(express.static(path.join(__dirname, '..', 'public')))
 
 module.exports = {
     app,
