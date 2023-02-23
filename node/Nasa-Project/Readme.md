83904960ea31a78259a5ca38f864a2d595766350
Yair <yair.notkovich@gmail.com>
2023-02-23 12:14:31 +0200

    test && jest

--- node/Nasa-Project/Readme.md
+++ node/Nasa-Project/Readme.md
@@ -1,15 +1,19 @@
 # Creating  Node API with express and React frontend
 
-build and run with `(front on port 8000)`  
+build and run `(front on port 8000)`  
 
 ```bash
 npm run deploy
 ```
 
-dev and run with `(front on port 3000)`  
+dev and run `(front on port 3000)`  
 
 ```bash
 npm run watch
 ```
 
 ![Diagram](Nasa-Project.png)
+
+## Testing
+
+* jest
---
+++ node/Nasa-Project/server/src/routes/launches/launches.test.js
@@ -0,0 +1,24 @@
+
+describe('Test GET /launches', () => {
+    test('Should respond with 200 success', () => {
+        const response = 200
+        expect(response).toBe(200)
+    })
+})
+
+describe('Test POST /launches', () => {
+    test('Should respond with 200 success', () => {
+        const response = 200
+        expect(response).toBe(200)
+    })
+
+    test('Should catch missing required properties', () => {
+        const response = 200
+        expect(response).toBe(200)
+    })
+
+    test('Should catch invalid dates', () => {
+        const response = 200
+        expect(response).toBe(200)
+    })
+})
